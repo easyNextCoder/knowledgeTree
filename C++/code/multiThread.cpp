1f9b7bdbffe7b00590aea1c36e2806dd7dd529a8
xuyongkang <2561258938@qq.com>
2020-04-05 20:22:09 +0800

    C++多线程、互斥量、条件变量学习

---
+++ C++/code/multiThread.cpp
@@ -0,0 +1,22 @@
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+void output(int i)
+{
+	cout << i << endl;
+}
+
+int main()
+{
+	
+	for (uint8_t i = 0; i < 4; i++)
+	{
+		thread t(output, i);
+		t.detach();	
+	}
+		
+	getchar();
+	return 0;
+}
