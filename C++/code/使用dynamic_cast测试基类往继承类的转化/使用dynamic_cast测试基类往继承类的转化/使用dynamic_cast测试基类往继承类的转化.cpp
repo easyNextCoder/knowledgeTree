423ec2a20b5897435f11605da5513151aadff382
xuyongkang <2561258938@qq.com>
2020-04-16 11:25:09 +0800

    C++:使用dynamic_cast实现往基类容器中放入子类

---
+++ C++/code/使用dynamic_cast测试基类往继承类的转化/使用dynamic_cast测试基类往继承类的转化/使用dynamic_cast测试基类往继承类的转化.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Window {
+public:
+	
+	virtual ~Window()
+	{
+		cout << "I am deconstructor of Window." << endl;
+	};
+};
+
+class SpecialWindow :public Window {
+public:
+	virtual void blink() {
+		cout << "I am blinking." << endl;
+	}
+	 void dosomething(){}
+	~SpecialWindow() {
+		cout << "I am deconstructor of SpecialWindow." << endl;
+	}
+};
+
+
+int main() {
+	vector<shared_ptr<Window> >vec;
+	vec.push_back(shared_ptr<Window>(new SpecialWindow()));
+	vec.push_back(shared_ptr<Window>(new SpecialWindow()));
+	vec.push_back(shared_ptr<Window>(new SpecialWindow()));
+
+	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+		SpecialWindow* psw =  dynamic_cast<SpecialWindow*>(iter->get());
+		psw->blink();
+	}
+
+
+	return 0;
+}
